922db8b12091df8b595d7d98b3c0acfbc153e5e0
Darrach Barneveld <darrachb1991@gmail.com>
2023-03-14 21:10:58 +0000

    removed memo

--- src/components/cards/EnemyFightCard.tsx
+++ src/components/cards/EnemyFightCard.tsx
@@ -1,6 +1,6 @@
 import imageJSON from "../../assets/images.json";
 import Image from "next/image";
-import { memo, useContext } from "react";
+import { useContext } from "react";
 import DeadOverlay from "../ui/DeadOverlay";
 import { FightContext } from "@/context/FightContext";
 import {
@@ -20,45 +20,47 @@ interface EnemyFightCardProps extends CharacterObjectProps {
   attackingCharacter: AttackingCharacter | undefined;
 }
 
-const EnemyFightCard: React.FC<EnemyFightCardProps> = memo(
-  ({ character, playerFightAnimation, attackingCharacter }) => {
-    const { turn } = useContext(FightContext);
-    const data = imageJSON.find((item) => item.name === character.name)!;
+const EnemyFightCard: React.FC<EnemyFightCardProps> = ({
+  character,
+  playerFightAnimation,
+  attackingCharacter,
+}) => {
+  const { turn } = useContext(FightContext);
+  const data = imageJSON.find((item) => item.name === character.name)!;
 
-    const { health } = characterHealthData.find(
-      (char) => char.name === character.name
-    )!;
+  const { health } = characterHealthData.find(
+    (char) => char.name === character.name
+  )!;
 
-    const percentage = 100 - (character.health / health) * 100;
+  const percentage = 100 - (character.health / health) * 100;
 
-    return (
-      <button
-        disabled={isOdd(turn) || character.health <= 0 || !attackingCharacter}
-        onClick={() => {
-          playerFightAnimation(character);
-        }}
-        className={styles.card}
-      >
-        <div className={styles.enemy}></div>
-        {character.health <= 0 && <DeadOverlay />}
-        <div className={styles.header}>
-          <h3>{character.name.substring(0, 20)}</h3>
-        </div>{" "}
-        <Image
-          alt={`Picture of the ${character.name}`}
-          width={400}
-          height={400}
-          className={styles.image}
-          src={data?.imageUrl}
-        />
-        <div className={styles.info}>
-          <HeartIcon percentage={percentage} id={character._id}>
-            <span className={styles.health}>{character.health}</span>
-          </HeartIcon>
-        </div>
-      </button>
-    );
-  }
-);
+  return (
+    <button
+      disabled={isOdd(turn) || character.health <= 0 || !attackingCharacter}
+      onClick={() => {
+        playerFightAnimation(character);
+      }}
+      className={styles.card}
+    >
+      <div className={styles.enemy}></div>
+      {character.health <= 0 && <DeadOverlay />}
+      <div className={styles.header}>
+        <h3>{character.name.substring(0, 20)}</h3>
+      </div>{" "}
+      <Image
+        alt={`Picture of the ${character.name}`}
+        width={400}
+        height={400}
+        className={styles.image}
+        src={data?.imageUrl}
+      />
+      <div className={styles.info}>
+        <HeartIcon percentage={percentage} id={character._id}>
+          <span className={styles.health}>{character.health}</span>
+        </HeartIcon>
+      </div>
+    </button>
+  );
+};
 
 export default EnemyFightCard;
